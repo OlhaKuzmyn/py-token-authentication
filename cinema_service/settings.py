766c44f11ea9a6137972e4a6bacaf19babc0d415
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-23 12:51:24 +0300

    added manage user option

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -120,13 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "user.User"
 
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
